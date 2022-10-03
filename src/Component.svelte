5ae7214d5411b6b3331d9434bcbba6680bcb72fb
Yuan Zhang <mitchellcheung1@yahoo.com>
2022-10-03 22:03:36 +0100

    Add validation and simplify code structure

--- src/Component.svelte
+++ src/Component.svelte
@@ -1,30 +1,43 @@
 <script>
   import { getContext } from "svelte"
 
-  export let text
-
   export let width = 640;
   export let height = 480;
 
   const { styleable } = getContext("sdk")
   const component = getContext("component")
-  export let url;
-  let src;
-  let urlWithPreview;
+  export let url = '';
 
-  function changeUrl(url) {
-    if (url.match("https://drive.google.com/drive/folders/")) {
-      urlWithPreview = url.replace("drive/folders/", "embeddedfolderview?id=")
-              .replace("?usp=sharing", "#list")
-    } else {
-      urlWithPreview = url.replace("/view?usp=sharing", "/preview");
+  const getIdFromUrl = (url) => {
+    const idWithExtraJunk = url.split('/')[5] ?? '';
+    return idWithExtraJunk.split('?')[0] ?? '';
+  };
+  const embedUrl = (id, type) => {
+    const embedFolder = (id) => `https://drive.google.com/embeddedfolderview?id=${id}#list`;
+    const embedFile = (id) => `https://drive.google.com/file/d/${id}/preview`;
+
+    if (type === 'folder') {
+      return embedFolder(id);
     }
-    return urlWithPreview
+    return embedFile(id);
   }
 
-  $: src = changeUrl(url);
+  function checkUrlValid(urlWithPreview) {
+    return !!(urlWithPreview.includes("https://drive.google.com/drive/folders/")
+            || urlWithPreview.includes("https://drive.google.com/file/"));
+  }
+
+  $: isValid = checkUrlValid(url);
+  $: fileType = url.includes('folders') ? 'folder' : 'file';
+  $: urlAsEmbedLink = embedUrl(getIdFromUrl(url), fileType);
 </script>
 
 <div use:styleable={$component.styles}>
-  <iframe src={src} width={width} height={height} allow="autoplay"></iframe>
+  {#if !url}
+    <p>Please enter a google drive URL.</p>
+  {:else if isValid}
+    <iframe src={urlAsEmbedLink} width={width} height={height} allow="autoplay"></iframe>
+  {:else}
+    <p>Error - Please enter a valid Google Drive url</p>
+  {/if}
 </div>
